727f445d0469704adbf830ff03f55bd42ebd3eac
noreebia <noreebia.gmail.com>
2015-04-30 14:27:07 +0900

    Added Server.java

---
+++ Server.java
@@ -0,0 +1,55 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.util.*;
+
+public class Server {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		try {
+			String input;
+			ServerSocket s=new ServerSocket(9090);
+			System.out.println("Server Running...");
+			Socket c=s.accept();
+			System.out.print("Connection established.");
+			PrintWriter out = new PrintWriter(c.getOutputStream(),true);
+			BufferedReader in=new BufferedReader(new InputStreamReader(c.getInputStream()));
+			BufferedReader stdIn =new BufferedReader( new InputStreamReader(System.in));
+			ServerThread st=new ServerThread(out,in,stdIn);
+			st.start();
+			while(true)
+			{
+				System.out.println(in.readLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Error");
+			e.printStackTrace();
+		}
+	}
+}
+
+class ServerThread extends Thread
+{
+	PrintWriter out;
+	BufferedReader in;
+	BufferedReader stdIn;
+	ServerThread(PrintWriter pw, BufferedReader br, BufferedReader br2)
+	{
+		this.out=pw;
+		this.in=br;
+		this.stdIn=br2;
+	}
+	public void run()
+	{
+		try {
+			while(true)
+			{
+				out.println(stdIn.readLine() );	
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+}
